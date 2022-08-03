5dc6373d70a3d23cae622d61d50c7ff9cd223107
Kailey <k.kotynski@gmail.com>
2022-08-03 12:44:34 -0700

    Added solution using hash table to twonumbersum.py

--- townumbersum.py
+++ townumbersum.py
@@ -9,3 +9,23 @@ def twoNumberSum(array, targetSum):
                 return [array[i],array[j]]
 
     return []
+
+# solution using hash table, O(n) runtime - from Leetcode
+
+def twoSum(self, nums, target):
+        # create a hash table to store nums present in array
+        nums_dict = {}
+        result = []
+        
+        # loop our array and calculate num needed, store num in nums dict
+        for i in range(len(nums)):
+            num_needed = target - nums[i]
+            # if num needed is in the dict, return the index of num and of num needed
+            if num_needed in nums_dict:
+                result.extend([i, nums.index(num_needed)])
+                return result
+            # otherwise, add num to the num dict    
+            else:
+                nums_dict[nums[i]] = True
+        return []
+        
